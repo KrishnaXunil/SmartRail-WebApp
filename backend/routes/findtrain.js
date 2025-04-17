b2bd0ecccc932bb06458dc0bbf0377db3c158bdd
vivek <gkrmvv3764@gmail.com>
2025-04-17 19:27:51 +0530

    added the train_search feature with location and changes the backend

--- backend/routes/findtrain.js
+++ backend/routes/findtrain.js
@@ -1,22 +1,8 @@
-const express = require("express");
-const {traininformation}=require("../controllers/findtrain")
+const express = require('express');
+const { traininformation } = require("../controllers/findtrain");
+const router = express.Router();
 
-const router=express.Router();
-
-// router.post('/',traininformation)
-
-router.get("/signup",(req,res)=>{
-    res.send("Signup Route");
-});
-
-router.get("/login",(req,res)=>{
-    res.send("login Route");
-});
+router.post("/", traininformation);
 
 
-router.get("/logout",(req,res)=>{
-    res.send("logout Route");
-});
-
-
-module.exports=router;
+module.exports = router;
